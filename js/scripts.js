02fc54491c0819d76405f0398d4db8510d27f2b2
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-18 22:40:40 -0800

    add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -22,17 +22,17 @@ var calculateNum = [
 function checkInput(userInputArray) {
   for (var i = 0; i < userInputArray.length; i++) {
     if (isNaN(userInputArray[i])) {
-      alert("user input of '" + userInputArray[i] + "[" + i + "]" + "' is NOT A NUMBER");
+      // alert("user input of '" + userInputArray[i] + "[" + i + "]" + "' is NOT A NUMBER");
       return false;
     }
     else if (i === 0) {
       if (lowRange > userInputArray[i] || userInputArray[i] > highRange) {
-        alert("user input of '" + userInputArray[i] + "' is not within options range of " + lowRange + " and " + highRange);
+        // alert("user input of '" + userInputArray[i] + "' is not within options range of " + lowRange + " and " + highRange);
         return false;
       }
     }
     else if (i === (userInputArray.length - 1)) {
-      alert("Each item in the array has been validated");
+      // alert("Each item in the array has been validated");
       return true;
     }
   }
@@ -41,7 +41,7 @@ function checkInput(userInputArray) {
 var cleanInput = checkInput(userInputArray);
 
 while (cleanInput === false) {
-  alert("userInput returned false");
+  // alert("userInput returned false");
   alert("invalid choice. TRY AGAIN!");
   var userInputArray = getInput();
   var num1= userInputArray[1];
@@ -50,53 +50,19 @@ while (cleanInput === false) {
   var cleanInput = checkInput(userInputArray);
 }
 if(cleanInput === true) {
-  alert("User input has been validated");
+  // alert("User input has been validated");
 }
 var calcSymb = ["+", "-", "/", "*"];
 
 var displayCalcOperation = function (num1, num2) {
-  alert(num1 + calcSymb[userChoice-1] + num2 + " = ");
-  var numValue = calculateNum[userChoice-1](num1, num2);
-  alert(numValue);
-
+  var numValue;
+  alert(num1 + calcSymb[userChoice-1] + num2 + " = " + (numValue = calculateNum[userChoice-1](num1, num2)));
 };
 
 displayCalcOperation(num1, num2);
 
-
-// var add = function(num1, num2) {
-//   // alert(num1 + calculatorSymbol[0] + num2 + calculatorSymbol[4]);
-//   return num1 + num2;
-// };
-// var subtract = function(num1, num2) {
-//   alert(num1 + " - " + num2 + " = ");
-//   return num1 - num2;
-// };
-// var divide = function(num1, num2) {
-//   alert(num1 + " / " + num2 + " = ");
-//   return num1 / num2;
-// };
-// var multiply = function(num1, num2) {
-//   alert(num1 + " x " + num2 + " = ");
-//   return num1 * num2;
-// };
-//
-// if (userChoice === 1) {
-//   var result = add(num1, num2);
-//   alert(result);
-// }
-// else if (userChoice === 2) {
-//   var result = subtract(num1, num2);
-//   alert(result);
-// }
-// else if (userChoice === 3) {
-//   var result = divide(num1, num2);
-//   alert(result);
-// }
-// else if (userChoice === 4) {
-//   var result = multiply(num1, num2);
-//   alert(result);
-// }
-// else {
-//   alert("UNKNOWN ERROR.");
-// }
+$(function() {
+  $(".secret").click(function() {
+    $(".clickable").toggle();
+  });
+});
